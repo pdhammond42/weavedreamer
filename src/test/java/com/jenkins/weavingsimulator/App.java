8175b104e9b4ba34412a76c8959e85726947b414
peterhammond <peter.hammond@users.sourceforge.net>
2015-04-19 11:58:57 +0100

    Build system upgrades to JDK1.8
    Using Java 8 requires building a pre-release UITest, repo now includes uispec4j-2.5-SNAPSHOT-jdk8.jar,
    built using jdk1.8.0_20 on Linux from https://github.com/UISPec4J/UISpec4J commit fdc0b42.
    Running this on Linux requires the Headless toolkit, see https://github.com/UISpec4J/UISpec4J/issues/19.
    
    UISpec4J brings in several dependencies, which were becoming tiresome to manage, so decided to move
    the build system over to Maven.

---
+++ src/test/java/com/jenkins/weavingsimulator/App.java
@@ -0,0 +1,13 @@
+package com.jenkins.weavingsimulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
